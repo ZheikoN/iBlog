4925e3c975c3fa1f0e2b1ac095bfa3100b2d3a3a
ZheikoN <negru.inger@gmail.com>
2022-10-25 12:33:41 +0000

    install allauth and connect signup and sign in pages

--- codestar/settings.py
+++ codestar/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
@@ -49,6 +53,11 @@ INSTALLED_APPS = [
     'blog',
 
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
