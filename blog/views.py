03433cefde5c8fd73b28b47df84d75be6698bfe3
ZheikoN <negru.inger@gmail.com>
2022-11-12 16:16:06 +0000

    add messaging system

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
+from django.contrib import messages
 
 
 class PostList(generic.ListView):
@@ -49,8 +51,10 @@ class PostDetail(View):
             comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
+            messages.success(request, "Your comment was posted successfully")
         else:
             comment_form = CommentForm()
+            messages.error(request, "Your comment was not posted, try again!")
 
         return render(
             request,
@@ -63,3 +67,17 @@ class PostDetail(View):
                 "liked": liked
             },
         )
+
+
+class PostLike(View):
+    
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+            messages.error(request, "you removed like")
+        else:
+            post.likes.add(request.user)
+            messages.success(request, "you added like")
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
